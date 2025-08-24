154bef3e3555c81cd04ca61c1f520d0116ff178f
sans80 <803medinajuanccb@gmail.com>
2025-08-24 18:54:39 -0500

    Add files via upload

---
+++ barra.php
@@ -0,0 +1,21 @@
+<aside class="admin-sidebar">
+            <h2>Gestión de Datos</h2>
+            <nav>
+                <ul>
+                    <li><a href="panel.html" class="active"><i class="fas fa-home"></i> Panel</a></li>
+                    <li><a href="admin_usuarios.html"><i class="fas fa-users"></i> Usuarios</a></li>
+                    <li><a href="admin_clientes.html"><i class="fas fa-user-friends"></i> Clientes</a></li>
+                    <li><a href="admin_empleados.html"><i class="fas fa-user-tie"></i> Empleados</a></li>
+                    <li><a href="admin_materia_prima.html"><i class="fas fa-boxes"></i> Materias Primas</a></li>
+                    <li><a href="admin_pedidos.html"><i class="fas fa-clipboard-list"></i> Pedidos</a></li>
+                    <li><a href="admin_bonos.html"><i class="fas fa-gift"></i> Bonos</a></li>
+                    <li><a href="admin_entrada_inventario.html"><i class="fas fa-truck-loading"></i> Entrada
+                            Inventario</a></li>
+                    <li><a href="admin_entrada_devolucion.html"><i class="fas fa-undo-alt"></i> Entrada Devolución</a>
+                    </li>
+                    <li><a href="admin_productos.html"><i class="fas fa-burger"></i> Productos</a></li>
+                    <li><a href="admin_facturas.html"><i class="fas fa-file-invoice"></i> Facturas</a></li>
+                    <li><a href="admin_redencion_bonos.html"><i class="fas fa-ticket-alt"></i> Redención Bonos</a></li>
+                </ul>
+            </nav>
+        </aside>
